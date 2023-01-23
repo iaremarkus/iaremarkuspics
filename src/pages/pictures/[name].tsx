0f7c3fe1f2103ccc64117281dc2892ccee63760e
Mark Bloomfield <mark@yellow-llama.com>
2023-01-23 21:58:38 +0200

    page meta

--- src/pages/pictures/[name].tsx
+++ src/pages/pictures/[name].tsx
@@ -23,23 +23,30 @@ export default function Home({ images }: Data) {
 
   return (
     <>
-      <Head>
-        <title>{name} | Pictures by iaremarkus | cc IG: iaremarkuspics</title>
-        <meta name="description" content="Pictures by iaremarkus | cc IG: iaremarkuspics" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-M7CFEQJ0J9" strategy="afterInteractive" />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+        strategy="afterInteractive"
+      />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
         
-          gtag('config', 'G-M7CFEQJ0J9');
+          gtag('config', '${process.env.GOOGLE_ANALYTICS}');
         `}
       </Script>
+
+      <Head>
+        <title>{name} | Pictures by iaremarkus | cc IG: iaremarkuspics</title>
+        <meta name="description" content="Pictures by iaremarkus | cc IG: iaremarkuspics" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="shortcut icon"
+          href="data:image/svg+xml,%3Csvg width='24' height='24' xmlns='http://www.w3.org/2000/svg' fill-rule='evenodd' clip-rule='evenodd'%3E%3Cstyle%3E path%7B stroke: %23000000; %7D @media (prefers-color-scheme: dark) %7B path%7B stroke: %23ffffff; %7D %7D %3C/style%3E%3Cpath d='M4.348 21.241l4.185-7.249 5.67 9.806c-.714.133-1.45.202-2.203.202-2.907 0-5.575-1.036-7.652-2.759zm18.97-5.247c-1.182 3.345-3.806 6.012-7.124 7.252l-4.187-7.252h11.311zm-14.786-6l-5.656 9.797c-1.793-2.097-2.876-4.819-2.876-7.791 0-.684.057-1.354.167-2.006h8.365zm12.583-5.795c1.798 2.098 2.885 4.824 2.885 7.801 0 .679-.057 1.345-.165 1.994h-8.373l5.653-9.795zm-11.305-3.999c.71-.131 1.442-.2 2.19-.2 2.903 0 5.566 1.033 7.642 2.751l-4.18 7.24-5.652-9.791zm2.19 7.794h-11.314c1.186-3.344 3.812-6.008 7.132-7.244l4.182 7.244z'/%3E%3C/svg%3E"
+          type="image/svg+xml"
+        />
+      </Head>
 
       <main className="p-4 md:p-10 lg:p-20 min-h-screen bg-gray-900">
         <div className="mb-2 md:mb-5 lg:mb-10">
