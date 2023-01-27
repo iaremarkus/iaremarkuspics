e225b1a74ba827bbfae1032cabeeb961b84f3f1f
Mark Bloomfield <mark@yellow-llama.com>
2023-01-27 20:36:06 +0200

    analytics and meta

--- app/root.tsx
+++ app/root.tsx
@@ -1,19 +1,43 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+  useLocation
+} from "@remix-run/react";
+import { useEffect } from "react";
+import * as gtag from "~/utils/gtags.client";
 
+import type { loader } from "./routes";
 import styles from "./styles/app.css";
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1"
-});
+export let meta: MetaFunction = () => {
+  return {
+    title: `Photos by iaremarkus | cc IG: iaremarkuspics`,
+    description: `Photography by iaremarkus. All photos are licensed under CC BY-NC-ND 4.0.`,
+    charset: "utf-8",
+    viewport: "width=device-width,initial-scale=1"
+  };
+};
 
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
 }
 
 export default function App() {
+  const location = useLocation();
+  const { gaTrackingId } = useLoaderData<typeof loader>();
+
+  useEffect(() => {
+    if (gaTrackingId?.length) {
+      gtag.pageview(location.pathname, gaTrackingId);
+    }
+  }, [location, gaTrackingId]);
+
   return (
     <html lang="en">
       <head>
--- app/routes/pictures.$name.tsx
+++ app/routes/pictures.$name.tsx
@@ -1,11 +1,30 @@
 import { ListObjectsCommand, S3Client } from "@aws-sdk/client-s3";
-import type { LoaderFunction } from "@remix-run/node";
+import type { LoaderFunction, MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import { useParams } from "react-router-dom";
 
 import { Footer, ModalImage } from "~/components";
+
+export let meta: MetaFunction = () => {
+  return {
+    title: `Photos by iaremarkus | cc IG: iaremarkuspics`,
+    description: `Photography by iaremarkus. All photos are licensed under CC BY-NC-ND 4.0.`,
+    charset: "utf-8",
+    viewport: "width=device-width,initial-scale=1"
+  };
+};
+
+export function links() {
+  return [
+    {
+      rel: "shortcut icon",
+      href: "data:image/svg+xml,%3Csvg width='24' height='24' xmlns='http://www.w3.org/2000/svg' fill-rule='evenodd' clip-rule='evenodd'%3E%3Cstyle%3E path%7B stroke: %23000000; %7D @media (prefers-color-scheme: dark) %7B path%7B stroke: %23ffffff; %7D %7D %3C/style%3E%3Cpath d='M4.348 21.241l4.185-7.249 5.67 9.806c-.714.133-1.45.202-2.203.202-2.907 0-5.575-1.036-7.652-2.759zm18.97-5.247c-1.182 3.345-3.806 6.012-7.124 7.252l-4.187-7.252h11.311zm-14.786-6l-5.656 9.797c-1.793-2.097-2.876-4.819-2.876-7.791 0-.684.057-1.354.167-2.006h8.365zm12.583-5.795c1.798 2.098 2.885 4.824 2.885 7.801 0 .679-.057 1.345-.165 1.994h-8.373l5.653-9.795zm-11.305-3.999c.71-.131 1.442-.2 2.19-.2 2.903 0 5.566 1.033 7.642 2.751l-4.18 7.24-5.652-9.791zm2.19 7.794h-11.314c1.186-3.344 3.812-6.008 7.132-7.244l4.182 7.244z'/%3E%3C/svg%3E",
+      type: "image/svg+xml"
+    }
+  ];
+}
 
 export const loader: LoaderFunction = async ({ params }) => {
   const s3Client = new S3Client({
@@ -37,7 +56,7 @@ export const loader: LoaderFunction = async ({ params }) => {
       return arr;
     }, []);
 
-  return json({ pictures });
+  return json({ pictures, gaTrackingId: process.env.GOOGLE_ANALYTICS });
 };
 
 export default function Index() {
